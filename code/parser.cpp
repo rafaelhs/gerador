6603776886da2bb28df0177957484d1c7b77c98d
rafael <rhirata.s@gmail.com>
2019-12-03 03:24:22 -0300

    implementado vetores, substituicao de constantes

--- code/parser.cpp
+++ code/parser.cpp
@@ -57,6 +57,26 @@ std::string getObjType(std::string line) {
     int i = 0;
     char c = ' ';
     std::string str = "";
+
+    if(line.at(0) == '(') {
+        i++;
+        c = line.at(i);
+        while(c != ')') {
+            i++;
+            c = line.at(i);
+        }
+        i++;
+        while(i < str.size()){
+            c = line.at(i);
+            str.append(1, c);
+            i++;
+        }
+        if(str == "++"){
+            return str;
+        }
+    }
+
+
     c = line.at(0);
     while(!(c >= 65 && c <= 90) && !(c >= 97 && c <= 122) && !(c >= '0' && c <= '9') &&  c != '_' &&
             c != '+' && c != '-' && c != '*' && c != '/' && c != '%' && c != '&' && c != '|' && 
@@ -97,6 +117,7 @@ int getNum(std::string str) {
     if(str == "EXIT") return EXIT;
     if(str == "RETURN") return RETURN;
     if(str == "+") return OPERATION;
+    if(str == "++") return OPERATION;
     if(str == "-") return OPERATION;
     if(str == "*") return OPERATION;
     if(str == "/") return OPERATION;
@@ -117,6 +138,7 @@ int getNum(std::string str) {
 
 int getOpType(std::string str) {
     if(str == "+") return OP_ADD;
+    if(str == "++") return OP_INC;
     if(str == "-") return OP_SUB;
     if(str == "*") return OP_MUL;
     if(str == "/") return OP_DIV;
@@ -136,6 +158,7 @@ int getOpType(std::string str) {
 
 int getOpGroup(std::string str) {
     if(str == "+") return ARITHMETICAL;
+    if(str == "++") return ARITHMETICAL;
     if(str == "-") return ARITHMETICAL;
     if(str == "*") return ARITHMETICAL;
     if(str == "/") return ARITHMETICAL;
@@ -160,6 +183,39 @@ bool isNumber(std::string str) {
     }
     return false;
 };
+
+std::vector<std::string> splitArray(std::string str) {
+    std::vector<std::string> tokens;
+    std:: string s = "";
+    char c;
+    int i = 0, flag = 0;;
+    
+    c = str.at(i);
+    while(c != '['){
+        s.append(1, c);
+        i++;
+        if(i >= str.size()) {
+            tokens.push_back(s);
+            return tokens;
+        }
+        c = str.at(i);
+    }
+    tokens.push_back(s);
+    s = "";
+    i++;
+    c = str.at(i);
+    while(c != ']'){
+        s.append(1, c);
+        i++;
+        if(i >= str.size()) {
+            tokens.push_back(s);
+            return tokens;
+        }
+        c = str.at(i);
+    }
+    tokens.push_back(s);
+    return tokens;
+}
 
 std::vector<std::string> split(std::string str) {
     std::vector<std::string> tokens;
@@ -264,11 +320,34 @@ std::vector<std::string> splitSemiCollon(std::string str) {
 }
 
 std:: vector<std::string> splitOperation(std::string str) {
-    std::string s = "";
+    std::string s = "", s2 = "";
     std::vector<std::string> tokens;
     int flag = 0;
     char c;
     int i = 0;
+
+    if(str.at(0) == '(') {
+        i++;
+        c = str.at(i);
+        while(c != ')') {
+            s.append(1, c);
+            i++;
+            c = str.at(i);
+        }
+        i++;
+        while(i < str.size()){
+            c = str.at(i);
+            s2.append(1, c);
+            i++;
+        }
+        tokens.push_back(s2);
+        tokens.push_back(s);
+        return tokens;
+    }
+
+
+
+
     c = str.at(i);
     while(c != '(') {
         s.append(1, c);
@@ -373,9 +452,17 @@ Constant* readConstant(std::string arg) {
 GlobalVariable* readGlobalVariable(std::string arg) {
     cout << "read global variable\n";
     GlobalVariable *gv = new GlobalVariable();
-    std::vector<std::string> splitLine = split(arg);
+    std::vector<std::string> splitLine = split(arg), arrSplit;
     gv->id = splitLine[2];
-    gv->type = splitLine[4];
+    arrSplit = splitArray(splitLine[4]);
+    gv->type = arrSplit[0];
+    if(arrSplit.size() > 1) {
+        gv->isArray = true;
+        gv->arraySize = stoi(arrSplit[1]);
+    }else{
+        gv->isArray = false;
+        gv->arraySize = 0;
+    }
     return gv;
 }
 
@@ -1075,25 +1162,35 @@ OpLeaf* readOpLeaf(std::string arg) {
     OpLeaf *opl = new OpLeaf(), *opl2;
     Operation *op;
     Variable *v;
+    GlobalVariable *gv;
+    Constant *cnst;
     Parameter *p;
     std::string vname;
     container *c;
     Function *f;
-    std::vector<std::string> param;
+    std::vector<std::string> param, arr;
     int i;
     
     vname = splitOperation(arg)[0];
-    c = AST->symbTable[vname]; //procura na global
+    arr = splitArray(vname);
+    c = AST->symbTable[arr[0]]; //procura na global
     if(c == NULL) { //caso nao encontrar
-        c = CURRENTFUNCTION->symbTable[vname]; //procura na local
+        c = CURRENTFUNCTION->symbTable[arr[0]]; //procura na local
     }
     if(c == NULL) { //caso nao encontrar em nenhuma das tabelas : constante
         //nao encontrado em nenhum lugar, e um valor constante
         opl->type = OP_CONSTANT;
         opl->valueType = INT;
-        opl->valueId = vname;
+        opl->valueId = arr[0];
     } else { //encontraddo em alguma das tabelas
         switch(c->type){
+            case CONSTANT:
+                cout << "!";
+                cnst = (Constant *)c->obj;
+                opl->type = OP_CONSTANT;
+                opl->valueType = INT;
+                opl->valueId = cnst->value;
+                break;
             case VARIABLE:
                 v = (Variable *)c->obj;
                 opl->type = OP_VARIABLE;
@@ -1103,6 +1200,31 @@ OpLeaf* readOpLeaf(std::string arg) {
                     opl->valueType = INT;
                 }
                 opl->valueId = v->id;
+                break;
+            case GLOBALVARIABLE:
+                gv = (GlobalVariable *)c->obj;
+                if(!gv->isArray){
+                    opl->type = OP_VARIABLE;
+                    if(gv->type == "char"){
+                        opl->valueType = CHAR;
+                    }else{
+                        opl->valueType = INT;
+                    }
+                    opl->valueId = gv->id;
+                }else{
+                    opl->type = OP_ARRAY;
+                    if(gv->type == "char"){
+                        opl->valueType = CHAR;
+                    }else{
+                        opl->valueType = INT;
+                    }
+                    opl->valueId = gv->id;
+                    opl2 = readOpLeaf(arr[1]);
+                    c = new container();
+                    c->type = OPLEAF;
+                    c->obj = opl2;
+                    opl->values.push_back(c);
+                }
                 break;
             case PARAMETER:
                 p = (Parameter *)c->obj;
@@ -1149,6 +1271,7 @@ OpLeaf* readOpLeaf(std::string arg) {
                 break;
         }
     }
+
     return opl;
 }
 
